80be63d0e19584119de51190044e8503cd740274
DavePaurillo <paurillo.dave@gmail.com>
2024-12-11 23:13:16 +0800

    Light and Dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of nextjs</h1>
+      {/* <h1 className="h1-bold">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold">Welcome to the world of nextjs</h1> */}
+      <h1 className="h1-bold pt-20">Welcome to the world of nextjs</h1>
     </>
   );
 }
