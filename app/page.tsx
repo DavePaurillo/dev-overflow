8965d360696b99512bf41bcafcb45f7325bdb656
DavePaurillo <paurillo.dave@gmail.com>
2024-12-11 15:26:29 +0800

    fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1 className="h1-bold">Welcome to the world of nextjs</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of nextjs
+      </h1>
+    </>
+  );
 }
 
 export default Home;
