993f26c5cbe4338d843b91a5656c7838bbb51c2c
DavePaurillo <paurillo.dave@gmail.com>
2024-12-12 23:45:51 +0800

    feat: Form auth layout, Dynamic display of sign up and sign out forms, zod

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { AuthForm } from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <AuthForm
+        formType="SIGN_IN"
+        schema={SignInSchema}
+        defaultValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(data) => {
+          return Promise.resolve({ success: true, data });
+        }}
+      />
+    </div>
+  );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { AuthForm } from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <AuthForm
+        formType="SIGN_UP"
+        schema={SignUpSchema}
+        defaultValues={{
+          email: "",
+          password: "",
+          name: "",
+          username: "",
+        }}
+        onSubmit={(data) => {
+          return Promise.resolve({ success: true, data });
+        }}
+      />
+    </div>
+  );
 }
