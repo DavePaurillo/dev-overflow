a4b6903a74f5939bf6a43e63810bda03e77b974e
DavePaurillo <paurillo.dave@gmail.com>
2024-12-17 01:24:13 +0800

    feat: Home filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -27,7 +28,7 @@ const questions = [
     title: "How to learn JavaScript",
     description: "I want to learn react, can someone help me with that?",
     tags: [
-      { _id: "1", name: "react" },
+      { _id: "1", name: "javascript" },
       { _id: "2", name: "javascript" },
     ],
     author: {
@@ -46,10 +47,16 @@ interface SearchParams {
 }
 
 async function Home({ searchParams }: SearchParams) {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
-    return question.title.toLowerCase().includes(query?.toLowerCase());
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
   });
 
   return (
@@ -69,7 +76,7 @@ async function Home({ searchParams }: SearchParams) {
           otherClasses="flex-1"
         />
       </section>
-      HomeFilter
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => {
           return <h1 key={question._id}>{question.title}</h1>;
