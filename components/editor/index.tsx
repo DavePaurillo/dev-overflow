c40c8751098a0ff7ecc703a19564b3e0e76dd273
DavePaurillo <paurillo.dave@gmail.com>
2024-12-17 23:02:39 +0800

    feat: QuestionForm Editor

---
+++ components/editor/index.tsx
@@ -0,0 +1,134 @@
+"use client";
+// InitializedMDXEditor.tsx
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  type MDXEditorMethods,
+  ConditionalContents,
+  toolbarPlugin,
+  ChangeCodeMirrorLanguage,
+  UndoRedo,
+  Separator,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  InsertCodeBlock,
+  linkPlugin,
+  linkDialogPlugin,
+  tablePlugin,
+  imagePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  diffSourcePlugin,
+} from "@mdxeditor/editor";
+import { basicDark } from "cm6-theme-basic-dark";
+import { useTheme } from "next-themes";
+import type { ForwardedRef } from "react";
+
+import "@mdxeditor/editor/style.css";
+import "./dark-editor.css";
+
+interface Props {
+  value: string;
+  fieldChange: (value: string) => void;
+  editorRef: ForwardedRef<MDXEditorMethods> | null;
+}
+
+export default function Editor({
+  value,
+  fieldChange,
+  editorRef,
+  ...props
+}: Props) {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "dark" ? [basicDark] : [];
+
+  return (
+    <MDXEditor
+      key={resolvedTheme}
+      markdown={value}
+      ref={editorRef}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
+      onChange={fieldChange}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        imagePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            css: "CSS",
+            html: "HTML",
+            javascript: "JavaScript",
+            json: "JSON",
+            markdown: "Markdown",
+            typescript: "TypeScript",
+            txt: "Plain Text",
+            sql: "SQL",
+            saas: "Sass",
+            scss: "Scss",
+            bash: "Bash",
+            tsx: "Typescript React",
+            jsx: "JavaScript React",
+            "": "unspecified",
+          },
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: theme,
+        }),
+        diffSourcePlugin({
+          viewMode: "rich-text",
+          diffMarkdown: "",
+        }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <ConditionalContents
+              options={[
+                {
+                  when: (editor) => editor?.editorType === "codeblock",
+                  contents: () => <ChangeCodeMirrorLanguage />,
+                },
+                {
+                  fallback: () => (
+                    <>
+                      <UndoRedo />
+                      <Separator />
+
+                      <BoldItalicUnderlineToggles />
+                      <Separator />
+
+                      <ListsToggle />
+                      <Separator />
+
+                      <CreateLink />
+                      <InsertImage />
+                      <Separator />
+
+                      <InsertTable />
+                      <InsertThematicBreak />
+
+                      <InsertCodeBlock />
+                    </>
+                  ),
+                },
+              ]}
+            />
+          ),
+        }),
+      ]}
+      {...props}
+    />
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,9 +58,7 @@ const config: Config = {
       },
     },
   },
-  // TODO
-  // plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 
 export default config;
